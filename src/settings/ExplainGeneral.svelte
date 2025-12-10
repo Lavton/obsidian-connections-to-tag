f59b5b4b03a725d436b156d0bb78d35701620142
Anton Lioznov <lavton@gmail.com>
2025-12-10 08:30:38 +0800

    add focusMaker settings and description

---
+++ src/settings/ExplainGeneral.svelte
@@ -0,0 +1,45 @@
+<h1>How it works?</h1>
+
+<p>
+	The plugin allows you to focus on a subset of your Vault (ex: one topic with
+	several notes).
+</p>
+
+<p>
+	"to focus" means to add all choosen files to <u>specific folder</u> (so you
+	can look at them all or just apply your "Ignore Filters" to ignore
+	everything except this folder). Or it means to add to all choosen files
+	<u>specific tag</u>: to look at the tag through search or to look at Graph
+	plugin.
+</p>
+
+<u>How to choose notes to focus on?</u>
+<p>
+	The plugin starts with one file/folder and distributes on links from the
+	file according to the rules you specify.
+</p>
+<p>To be flexible, plugin works with 3 abstractions:</p>
+<ul>
+	<li>
+		<code>Connection</code> - how to choose neighbour files from given one. Maybe
+		all links are just 'neighbour', but the link with 'parent' tag indicates structure?
+	</li>
+	<li>
+		<code>Rule</code> - how deep to go from the starting file? Till the end or
+		just a few steps?
+	</li>
+	<li>
+		<code>Chain step</code> - combination of "connection" and "rule": from this
+		starting note will look at all neighbours according to "Connection" and go
+		there if "Rule" told to
+	</li>
+</ul>
+
+<hr>
+Table of content
+<ul>
+<li><a href="#section-focuser">What to do with found notes</a></li>
+<li><a href="#section-chain">What rules and chains to apply</a></li>
+<li><a href="#section-ui">UI settings</a></li>
+
+</ul>
--- src/settings/settings.ts
+++ src/settings/settings.ts
@@ -1,16 +1,17 @@
 import { App, PluginSettingTab, Plugin, Setting } from "obsidian";
 
+import { mount } from "svelte";
+import ExplainGeneral from './ExplainGeneral.svelte';
 export interface ResultsSettings {
 	workingTag: string;
-	savedFilters: any;
 	// goalFolder: string,
 	// autoAddToFolder: boolean; 
 	// reverseFrontmatter: string;
 }
 
 export enum MarkNoteMode {
-	ADD_TAG,
-	MOVE_TO_FOLDER
+	ADD_TAG = "add_tag",
+	MOVE_TO_FOLDER = "move_to_folder"
 }
 
 export interface FocusMakerSettings {
@@ -21,11 +22,11 @@ export interface FocusMakerSettings {
 }
 
 export interface ConnectionsToTagSettings {
-    workingTag: string, // deprecated
-	resultsSettings: ResultsSettings,
-    parentsTag: string[],
-    aroundNumber: number,
-    isFirstTagLineParentWhenEmpty: boolean,
+	workingTag: string, // deprecated
+	focusMakerSettings: FocusMakerSettings,
+	parentsTag: string[],
+	aroundNumber: number,
+	isFirstTagLineParentWhenEmpty: boolean,
 }
 
 
@@ -50,13 +51,22 @@ export const NEW_DEFAULT_SETTINGS = {
 }
 
 export const DEFAULT_SETTINGS: ConnectionsToTagSettings = {
-    workingTag: "#to_focus_on",
-	resultsSettings: {
-		workingTag: "#to_focus_on",
-	},
-    parentsTag: ["parents"],
-    aroundNumber: 0,
-    isFirstTagLineParentWhenEmpty: true,
+	workingTag: "#to_focus_on",
+	// resultsSettings: {
+	// 	workingTag: "#to_focus_on",
+	// },
+	parentsTag: ["parents"],
+	aroundNumber: 0,
+	isFirstTagLineParentWhenEmpty: true,
+	focusMakerSettings: {
+		resultTag: "to_focus_on",
+		resultFolder: "focusNotes/",
+		movedNameFrontmatter: "moved_from",
+		markNoteModes: [
+			MarkNoteMode.ADD_TAG,
+			// MarkNoteMode.MOVE_TO_FOLDER
+		],
+	}
 }
 
 
@@ -78,59 +88,127 @@ export class ConnectionsToTagSettingTab extends PluginSettingTab {
 		const { containerEl } = this;
 
 		containerEl.empty();
+		const explain = mount(ExplainGeneral, {
+			target: containerEl,
+			props: {
+			}
+		});
+		const section1 = containerEl.createDiv({ cls: 'settings-section' });
+		section1.id = 'section-focuser';
+		section1.createEl('h2', { text: 'What to do with found notes' });
+		// focusMaker settings. 
+		new Setting(containerEl)
+			.setName("Apply tag / move to folder")
+			.setDesc("What will be the result of applying chain? All notes will have specific tag or will be moved to specific folder?")
+			.addToggle(toggle => toggle
+				.setValue(this.plugin.settings.focusMakerSettings.markNoteModes.contains(MarkNoteMode.MOVE_TO_FOLDER))
+				.onChange(async (value) => {
+					if (value) {
+						this.plugin.settings.focusMakerSettings.markNoteModes = [MarkNoteMode.MOVE_TO_FOLDER];
+					} else {
+						this.plugin.settings.focusMakerSettings.markNoteModes = [MarkNoteMode.ADD_TAG];
+					}
+					await this.plugin.saveSettings();
+				})
+			);
+		new Setting(containerEl)
+			.setName("Result tag")
+			.setDesc("What tag will be applying in case of 'apply tag'? (Use #hastag to apply it in file text and without # to apply into frontamatter tags)")
+			.addText((text) => {
+				text
+					.setPlaceholder('ex. #to_focus_on')
+				.setValue(this.plugin.settings.focusMakerSettings.resultTag)
+				.onChange(async (value) => {
+					this.plugin.settings.focusMakerSettings.resultTag = value
+					await this.plugin.saveSettings();
+				})
+			})
+		new Setting(containerEl)
+			.setName("Result folder")
+			.setDesc("In what folder the notes will be moved to?")
+			.addText((text) => {
+				text
+					.setPlaceholder('ex. focusNotes/')
+				.setValue(this.plugin.settings.focusMakerSettings.resultFolder)
+				.onChange(async (value) => {
+					this.plugin.settings.focusMakerSettings.resultFolder = value
+					await this.plugin.saveSettings();
+				})
+			})
+		new Setting(containerEl)
+			.setName("Technical 'moved' frontmatter")
+			.setDesc("Add frontmatter that will be used to store original location of the notes when it moved to folder")
+			.addText((text) => {
+				text
+					.setPlaceholder('ex. moved_from')
+				.setValue(this.plugin.settings.focusMakerSettings.movedNameFrontmatter)
+				.onChange(async (value) => {
+					this.plugin.settings.focusMakerSettings.movedNameFrontmatter = value
+					await this.plugin.saveSettings();
+				})
+			})
+		// TODO: settings result folder, moved_from_fronmatter. Потом переключиться на эти
+		const section2 = containerEl.createDiv({ cls: 'settings-section' });
+		section2.id = 'section-chain';
+		section2.createEl('h2', { text: 'What rules and chains to apply' });
+		// chains & traversal
+		const section3 = containerEl.createDiv({ cls: 'settings-section' });
+		section3.id = 'section-ui';
+		section3.createEl('h2', { text: 'UI settings' });
+		// UI
 
-		new Setting(containerEl)
-			// as in https://github.com/zsviczian/excalibrain/blob/master/src/Settings.ts
-			.setName('working tag')
-			.setDesc('notes with what tag will be moved')
-			.addText((text) => {
-				text
-				.setPlaceholder('ex. #to_focus_on')
-				.setValue(this.plugin.settings.workingTag)
-				.onChange(async (value) => {
-					this.plugin.settings.workingTag = value
-					if (!(value.startsWith("#"))) {
-						this.plugin.settings.workingTag = "#" + this.plugin.settings.workingTag
-					}
-					await this.plugin.saveSettings();
-				})
-			})
-		new Setting(containerEl)
-		.setName('parents tags')
-		.setDesc('tags of parents, sep by comma')
-			.addText((text) => {
-				text
-				.setValue(this.plugin.settings.parentsTag.join(", "))
-				.onChange(async (value) => {
-					var tagNames = value.split(",").map((t) => t.trim())
-					this.plugin.settings.parentsTag = tagNames
-					await this.plugin.saveSettings();
-				})
-			})
-		new Setting(containerEl)
-		.setName('number of neibors')
-		.setDesc('Number of nodes-neibors that will be connected')
-			.addText((text) => {
-				text
-				.setValue(this.plugin.settings.aroundNumber.toString())
-				.onChange(async (value) => {
-					var num = parseInt(value)
-					if (!Number.isNaN(num)) {
-						this.plugin.settings.aroundNumber = num
-						await this.plugin.saveSettings();
-					}
-				})
-			})
-		new Setting(containerEl)
-		.setName('use first line as parents')
-		.setDesc('if no tags are inside, use first mention nodes as parents')
-		.addToggle((cb) => {
-			cb 
-			.setValue(this.plugin.settings.isFirstTagLineParentWhenEmpty)
-			.onChange(async (value) => {
-				this.plugin.settings.isFirstTagLineParentWhenEmpty = value
-				await this.plugin.saveSettings();
-			})
-		})
+		// new Setting(containerEl)
+		// 	// as in https://github.com/zsviczian/excalibrain/blob/master/src/Settings.ts
+		// 	.setName('working tag')
+		// 	.setDesc('notes with what tag will be moved')
+		// 	.addText((text) => {
+		// 		text
+		// 		.setPlaceholder('ex. #to_focus_on')
+		// 		.setValue(this.plugin.settings.workingTag)
+		// 		.onChange(async (value) => {
+		// 			this.plugin.settings.workingTag = value
+		// 			if (!(value.startsWith("#"))) {
+		// 				this.plugin.settings.workingTag = "#" + this.plugin.settings.workingTag
+		// 			}
+		// 			await this.plugin.saveSettings();
+		// 		})
+		// 	})
+		// new Setting(containerEl)
+		// .setName('parents tags')
+		// .setDesc('tags of parents, sep by comma')
+		// 	.addText((text) => {
+		// 		text
+		// 		.setValue(this.plugin.settings.parentsTag.join(", "))
+		// 		.onChange(async (value) => {
+		// 			var tagNames = value.split(",").map((t) => t.trim())
+		// 			this.plugin.settings.parentsTag = tagNames
+		// 			await this.plugin.saveSettings();
+		// 		})
+		// 	})
+		// new Setting(containerEl)
+		// .setName('number of neibors')
+		// .setDesc('Number of nodes-neibors that will be connected')
+		// 	.addText((text) => {
+		// 		text
+		// 		.setValue(this.plugin.settings.aroundNumber.toString())
+		// 		.onChange(async (value) => {
+		// 			var num = parseInt(value)
+		// 			if (!Number.isNaN(num)) {
+		// 				this.plugin.settings.aroundNumber = num
+		// 				await this.plugin.saveSettings();
+		// 			}
+		// 		})
+		// 	})
+		// new Setting(containerEl)
+		// .setName('use first line as parents')
+		// .setDesc('if no tags are inside, use first mention nodes as parents')
+		// .addToggle((cb) => {
+		// 	cb 
+		// 	.setValue(this.plugin.settings.isFirstTagLineParentWhenEmpty)
+		// 	.onChange(async (value) => {
+		// 		this.plugin.settings.isFirstTagLineParentWhenEmpty = value
+		// 		await this.plugin.saveSettings();
+		// 	})
+		// })
 	}
 }
